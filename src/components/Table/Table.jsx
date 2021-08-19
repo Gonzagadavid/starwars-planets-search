625fcd4566db8b06e2d2c2d420ba93bead80eb3c
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-08-18 21:26:19 -0300

    corrigido nomes de constantes

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,26 +3,26 @@ import StarWarsContext from '../../context/StarWarsContext';
 
 const Table = () => {
   const { data } = useContext(StarWarsContext);
-  if (!data.length) return <p>Loading</p>;
+  if (!data.length) return <p>Loading...</p>;
 
-  const heads = Object.keys(data[0]).filter((key) => key !== 'residents');
+  const tableKeys = Object.keys(data[0]).filter((key) => key !== 'residents');
 
   return (
     <table>
       <thead>
         <tr>
-          {heads.map((head) => <th key={ head }>{head}</th>)}
+          {tableKeys.map((tableKey) => <th key={ tableKey }>{tableKey}</th>)}
         </tr>
       </thead>
       <tbody>
         {data.map((infoList) => (
           <tr key={ infoList.name }>
-            {heads.map((head) => (
+            {tableKeys.map((tableKey) => (
               <td
-                key={ infoList[head] }
-                data-testid={ head === 'name' ? 'planet-name' : '' }
+                key={ infoList[tableKey] }
+                data-testid={ tableKey === 'name' ? 'planet-name' : '' }
               >
-                {infoList[head]}
+                {infoList[tableKey]}
               </td>
             ))}
           </tr>
